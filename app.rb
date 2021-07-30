2f30fc18436def8884095144b0b6826ad9956ea8
mrabets <newrabgo@gmail.com>
2021-07-30 18:24:52 +0300

    Add post method for add_post

--- app.rb
+++ app.rb
@@ -9,3 +9,10 @@ end
 get '/add_post' do
   erb :add_post
 end
+
+post '/add_post' do
+  @title = params[:title]
+  @content = params[:content]
+
+  erb "Your title: #{@title}\ncontent: #{@content}"
+end
