0c31cebf30333c1440142826308bb7fbd39b05b7
mrabets <newrabgo@gmail.com>
2021-08-02 14:10:30 +0300

    Refactoring

--- app.rb
+++ app.rb
@@ -4,13 +4,12 @@ require 'sinatra/reloader'
 require 'sinatra/activerecord'
 require 'sqlite3'
 
-set :database, { adapter: 'sqlite3', database: 'blog.db' }
+set :database,  adapter: 'sqlite3', database: 'blog.db'
 
 class Post < ActiveRecord::Base
-  validates :author, presence: true, length: { minimum: 2 }
-  validates :title, presence: true, length: { maximum: 200 }
-  validates :content, presence: true
-  validates :datestamp, presence: true
+  validates :author, :title, :content, presence: true
+  validates :author, length: { minimum: 2 }
+  validates :title, length: { maximum: 200 }
 end
 
 class Comment < ActiveRecord::Base
@@ -18,25 +17,6 @@ class Comment < ActiveRecord::Base
 end
 
 configure do
-  $db = SQLite3::Database.new 'base.db'
-  $db.execute 'CREATE TABLE IF NOT EXISTS "Posts"
-    (
-      "Id" INTEGER,
-      "Title" TEXT,
-      "Content"	TEXT,
-      "Author" TEXT,
-      "DateTime" DATE,
-      PRIMARY KEY("Id" AUTOINCREMENT)
-    )'
-
-  $db.execute 'CREATE TABLE IF NOT EXISTS "Comments"
-    (
-      "Id" INTEGER,
-      "DateTime" DATE,
-      "Content"	TEXT,
-      "PostId" INTEGER,
-      PRIMARY KEY("Id" AUTOINCREMENT)
-    )'
 end
 
 helpers do
@@ -62,7 +42,6 @@ post '/add_post' do
     @error = @post.errors.full_messages.first
     erb :add_post
   end
-
 end
 
 get '/posts' do
@@ -71,24 +50,16 @@ end
 
 get '/post/:id' do
   @post = Post.find params[:id]
-
   erb :post_details
 end
 
 post '/post/:id' do
-  post_id = params[:id]
-  content = params[:content]
-
-  comment = Comment.new
-  comment.post_id = post_id
-  comment.content = content
-  comment.datestamp = DateTime.current.to_date
+  comment = Comment.new post_id: params[:id], content: params[:content], datestamp: DateTime.current.to_date
 
   if comment.save
-    redirect to('/post/' + post_id)
+    redirect to('/post/' + params[:id])
   else
     @error = comment.errors.full_messages.first
     erb :post_details
   end
-
 end
